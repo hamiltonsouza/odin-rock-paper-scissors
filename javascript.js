62e5f59ae29f36674f3d90b234a57f6d82cfb2c6
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2024-01-08 17:47:16 -0300

    Add computer selection logic and draw return

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
-console.log("Hello, World!")
+function getComputerChoice() {
+
+    const computerOptions = ["rock","paper","scissors"];
+    const computerSelection = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+    return computerSelection;
+
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection === computerSelection) {
+       return "It's a draw!";
+    } else {
+        return playerSelection;
+    }
+
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
