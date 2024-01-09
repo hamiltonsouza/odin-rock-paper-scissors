5244d95fd71d033925ac19389fba879d6c02fa65
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2024-01-09 14:49:32 -0300

    Add game function and move choices into rounds

--- javascript.js
+++ javascript.js
@@ -15,16 +15,35 @@ function getPlayerChoice() {
 
 function playRound(playerSelection, computerSelection) {
 
+    playerSelection = getPlayerChoice();
+    computerSelection = getComputerChoice();
+
     if (playerSelection === computerSelection) {
-       return "It's a Draw!";
+        return "It's a Draw!";
     } else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock") {
+        playerScore = playerScore + 1;
         return `You Win! ${playerSelection[0].toUpperCase() + playerSelection.substring(1)} beats ${computerSelection}!`;
     } else {
+        computerScore = computerScore + 1;
         return `You Lose! ${computerSelection[0].toUpperCase() + computerSelection.substring(1)} beats ${playerSelection}!`;
     }
 
 }
 
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    while (playerScore < 3 && computerScore < 3) {
+        console.log(playRound(playerSelection,computerSelection));
+    }
+
+    if (playerScore === 3) {
+        return `Congratulations! You've won the game! ${playerScore} to ${computerScore}`
+    } else {
+        return `Too bad! You lost to the computer ${computerScore} to ${playerScore}`
+    }
+}
+
+let playerSelection = '';
+let playerScore = 0;
+let computerSelection = '';
+let computerScore = 0;
+console.log(game());
